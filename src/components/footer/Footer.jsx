a3f620c6562194ec5c17a94666d0b08149af95fd
Rohan Reddy Melachervu <rohan.reddy.melachervu@gmail.com>
2022-12-30 16:42:30 +0530

    Changed footer and added more footer links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './footer.css'
 import {SlSocialInstagram} from 'react-icons/sl'
+import {BsLinkedin} from 'react-icons/bs'
+import {AiOutlineGithub} from 'react-icons/ai'
+import {SiLeetcode} from 'react-icons/si'
 const Footer = () => {
   return (
     <footer>
@@ -12,6 +15,9 @@ const Footer = () => {
       </ul>
       <div className="footer__socials">
         <a href="https://www.instagram.com/rohan._.reddy/" target="_blank" rel="noreferrer"><SlSocialInstagram /></a>
+        <a href="https://www.linkedin.com/in/rohan-reddy-melachervu-326b74219/" target="_blank"><BsLinkedin /></a>
+        <a href="https://github.com/rohanreddymelachervu" target="_blank"><AiOutlineGithub /></a>
+        <a href="https://leetcode.com/bruhan_reddy" target="_blank"><SiLeetcode /></a>
       </div>
     </footer>
   )
