bb0a956156dc65535755ac1fb03ddbc23e105669
Rohan Reddy Melachervu <rohan.reddy.melachervu@gmail.com>
2023-02-02 17:42:56 +0530

    Added animations

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,27 @@ import About from './components/about/About'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 import Fact from './components/fact/Fact'
+import Aos from 'aos'
+import "aos/dist/aos.css"
+import { useEffect } from 'react'
 const App = () => {
+  useEffect(() => {
+    Aos.init({duration: 1500, once:true});
+  }, []);
   return (
     <div>
       <Header />
-      <Fact />
+      <div data-aos="fade-right">
+        <Fact />
+        <About />
+      </div>
       <Nav />
-      <About />
-      <Experience />
-      <Contact />
+      <div data-aos="fade-left">
+        <Experience />
+      </div>
+      <div data-aos="flip-up">
+        <Contact />
+      </div>
       <Footer />
     </div>
   )
